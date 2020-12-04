557c4c8bd39a2c421176579216e11bb0730f30c9
Rikke Bendlin Gammelmark <rikke@toitware.com>
2020-12-04 11:04:08 +0100

    Removing function page and link from class info. Change font to Roboto.

--- src/components/methods.tsx
+++ src/components/methods.tsx
@@ -4,29 +4,10 @@ import React from "react";
 import Toitdocs from "./toitdoc_info";
 import { ArrowRightAlt } from "@material-ui/icons";
 import Box from "@material-ui/core/Box";
-import { Link } from "react-router-dom";
 import { Parameters } from "./parameters";
 import { Type } from "./util";
 import { ToitFunction } from "../model/toitsdk";
-
-function ConditionalLink(props: {
-  function: ToitFunction;
-  functionIndex: number;
-  props: MethodsProps;
-}): JSX.Element {
-  const restrictedSigns = ["/", "%"];
-  if (!restrictedSigns.includes(props.function.name)) {
-    return (
-      <Link
-        to={`/${props.props.libName}/${props.props.moduleName}/${props.props.className}/${props.props.functionType}/${props.function.name}/${props.functionIndex}`}
-      >
-        <strong>{`${props.function.name} `}</strong>
-      </Link>
-    );
-  } else {
-    return <strong>{`${props.function.name} `}</strong>;
-  }
-}
+import { Typography } from "@material-ui/core";
 
 export type FunctionType = "Constructors" | "Statics" | "Factories" | "Methods";
 
@@ -48,28 +29,22 @@ function Methods(props: MethodsProps): JSX.Element {
           // Alternating background.
           const background = i % 2 ? "#eeeeee" : "#fafafa";
           return (
-            <div key={`${fn.name}_${i}`}>
-              <Box p={1} bgcolor={background} borderRadius={8}>
-                <span className="functionName">
-                  <ConditionalLink
-                    props={props}
-                    function={fn}
-                    functionIndex={i}
-                  />
-                </span>
-                <Parameters parameters={fn.parameters} />
-                <ArrowRightAlt
-                  style={{
-                    verticalAlign: "middle",
-                    display: "inline-flex",
-                  }}
-                />
-                <span>
-                  <Type type={fn.return_type}></Type>
-                </span>
-                <Toitdocs value={fn.toitdoc} />
-              </Box>
-            </div>
+            <Box p={1} bgcolor={background} borderRadius={8} key={"method" + i}>
+              <Typography variant="h6" component="span">
+                {fn.name}
+              </Typography>
+              <Parameters parameters={fn.parameters} />
+              <ArrowRightAlt
+                style={{
+                  verticalAlign: "middle",
+                  display: "inline-flex",
+                }}
+              />
+              <span>
+                <Type type={fn.return_type}></Type>
+              </span>
+              <Toitdocs value={fn.toitdoc} />
+            </Box>
           );
         })}
     </>
