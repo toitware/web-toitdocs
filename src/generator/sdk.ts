86cf9c3edc4532808b4e9e77c9575343fc077796
Florian Loitsch <florian@toitware.com>
2021-04-26 16:21:21 +0200

    Make toitdoc optional.

--- src/generator/sdk.ts
+++ src/generator/sdk.ts
@@ -69,7 +69,7 @@ export interface ToitModule {
   export_globals: ToitGlobal[];
   functions: ToitFunction[];
   export_functions: ToitFunction[];
-  toitdoc: ToitDoc;
+  toitdoc?: ToitDoc;
 }
 
 export interface ToitModules {
