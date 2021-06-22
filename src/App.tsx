76092c63cd4f42aa8e243ffeb53807c3bd7efd50
qvisten999 <jqjqvist@gmail.com>
2021-06-22 14:10:36 +0200

    added crisp

--- src/App.tsx
+++ src/App.tsx
@@ -75,6 +75,15 @@ class App extends Component<AppProps> {
     this.props.fetchSdk(this.props.sdkVersionFromParams);
   }
 
+  setupCrispChat = () => {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "d7358065-35d6-43ee-bcd9-608d223d7aab";
+    const s = document.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = true;
+    document.getElementsByTagName("head")[0].appendChild(s);
+  };
+
   render(): JSX.Element {
     let segmentAPIKey = "";
     if (typeof document !== `undefined`) {
@@ -155,6 +164,9 @@ class App extends Component<AppProps> {
           segmentKey={segmentAPIKey}
           changeConsent={false}
           show={true}
+          onAnalyticsReady={() => {
+            this.setupCrispChat();
+          }}
         />
       </ThemeProvider>
     );
